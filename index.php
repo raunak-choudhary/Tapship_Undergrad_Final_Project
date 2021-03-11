5c8836fd0b25e4ae6e661ce013678d24776447ff
Ganpat Patel <ganpat.patel.012@gmail.com>
2021-03-11 22:23:56 +0530

    Done

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html>
-<!--Hello Raunak-->
-<!--Hello Babaji-->
-<!-- 2nd Edit Done-->
-<!-- babaji aa gaye Edit Done-->
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
