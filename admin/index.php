153bd106006ff764711f5c07414c3e465c181777
Suraj Prakash Gautam <spgautam.vfx@gmail.com>
2021-03-21 21:30:01 +0530

    Footer Fixed

--- admin/index.php
+++ admin/index.php
@@ -533,37 +533,9 @@ header("location: login.php"); // Redirecting To Profile Page
         </div>
       </div>
       <footer class="footer">
-        <div class="container-fluid">
-          <nav class="float-left">
-            <ul>
-              <li>
-                <a href="https://www.creative-tim.com">
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a href="https://creative-tim.com/presentation">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a href="http://blog.creative-tim.com">
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a href="https://www.creative-tim.com/license">
-                  Licenses
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div class="copyright float-right">
-            &copy;
-            <script>
-              document.write(new Date().getFullYear())
-            </script>, made with <i class="material-icons">favorite</i> by
-            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+        <div class="container-fluid bg-success pt-3">
+          <div class="container">
+            <p style="text-align: center;"><strong>© 2020 TapShip.&nbsp; All rights reserved.</strong><br></p>
           </div>
         </div>
       </footer>
