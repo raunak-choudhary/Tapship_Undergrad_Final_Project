76d08428946103226e2b462a3b90dfe8d12dd13b
Faheem Ahmad <42387113+faheemahmad227@users.noreply.github.com>
2021-03-22 13:29:29 +0530

    Update farmerprofile.php

--- admin/farmerprofile.php
+++ admin/farmerprofile.php
@@ -6,33 +6,37 @@ $a_name= $res;
 if(!isset($_SESSION['login_admin'])){
 header("location: login.php"); // Redirecting To Profile Page
 }
-?> 
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Farmer Profile View</title>
-    <link rel="icon" href="../assets/img/fav.png" type="image/png">
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
-    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="../assets/css/Article-List.css">
-    <link rel="stylesheet" href="../assets/css/Features-Boxed.css">
-    <link rel="stylesheet" href="../assets/css/Footer-Dark.css">
-    <link rel="stylesheet" href="../assets/css/Highlight-Blue.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.css">
-    <link rel="stylesheet" href="../assets/css/Login-Form-Clean.css">
-    <link rel="stylesheet" href="../assets/css/table-style.css"/>
-</head>
-
-<body id="page-top">
-    <nav class="navbar navbar-light navbar-expand-lg fixed-top text-uppercase" id="mainNav" style="background: #0c3823;">
+?>
+<!doctype html>
+        <html>
+            <head>
+                <meta charset='utf-8'>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+				<title>Driver Profile View</title>
+				<link rel="icon" href="../assets/img/fav.png" type="image/png">
+				<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
+				<link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+				<link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
+				<link rel="stylesheet" href="../assets/css/Article-List.css">
+				<link rel="stylesheet" href="../assets/css/Footer-Dark.css">
+				<link rel="stylesheet" href="../assets/css/Highlight-Blue.css">
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.css">
+				<link rel="stylesheet" href="../assets/css/Login-Form-Clean.css">
+				<link rel="stylesheet" href="../assets/css/table-style.css">
+                <link rel="stylesheet" href="../assets/css/profile.css">
+                <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet'>
+                <link href='https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/3.6.95/css/materialdesignicons.css' rel='stylesheet'>
+                                
+                <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+                <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'></script>
+                <script type='text/javascript'></script>
+            </head>
+        <body oncontextmenu='return false' class='snippet-body'>
+        <div class="page-content page-container" id="page-content">
+		<nav class="navbar navbar-light navbar-expand-lg fixed-top text-uppercase" id="mainNav" style="background: #0c3823;">
         <div class="container">
             <a class="navbar-brand js-scroll-trigger" data-bs-hover-animate="pulse" href="../index.php" style="font-family: Montserrat, sans-serif;">TAPSHIP</a>
             <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right text-uppercase rounded" data-aos="fade" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="background: #fff;" ><i class="fa fa-bars" style="color: #0c3823;;"></i></button>
@@ -50,8 +54,7 @@ header("location: login.php"); // Redirecting To Profile Page
             </div>
         </div>
     </nav>
-
-<?php 
+	<?php 
 $con=mysqli_connect("localhost","root","","tapship");
    if(!$con)
    {
@@ -65,7 +68,7 @@ $con=mysqli_connect("localhost","root","","tapship");
 
    while( $res=mysqli_fetch_assoc($result))
    {
-       $f_id =  $res['f_id'];
+      $f_id =  $res['f_id'];
        $f_name =  $res['f_name'];
  
        $f_gender =  $res['f_gender'];
@@ -83,6 +86,8 @@ $con=mysqli_connect("localhost","root","","tapship");
        $f_approve =  $res['f_approve'];
    }
 ?>
+
+
     <div class="features-boxed">
         <div class="container" style="background: #ffffff;">
             <div class="intro" style="background: #0c3823;margin-top: 120px;margin-bottom: 30px;">
@@ -92,110 +97,153 @@ $con=mysqli_connect("localhost","root","","tapship");
     </div>
 
 
-    <div class="container">
-
-    <?php
-    if($f_type=="wholesaler")
-    {
-    ?>
-    <p><img src="../farmers/<?php echo  $f_photo;?>" width="200" height="240"></p>
- 
-    <p>ID: <?php echo "$f_id"?></p>
-    <p>Name: <?php echo "$f_name"?></p>
-    <p>Mobile: <?php echo "$f_mobile"?></p>
-    <p>Gender: <?php echo "$f_gender"?></p>
-    <p>Age: <?php echo "$f_age"?></p>
-    <p>Street: <?php echo "$f_street"?></p>
-    <p>City: <?php echo "$f_city"?></p>
-    <p>State: <?php echo "$f_state"?></p>
-    <p>Pincode: <?php echo "$f_pincode"?></p>
-    <p>Aadhar: <?php echo "$f_aadhar"?></p>
-    <p>Aadhar PDF: <button class="btn btn-dark text-monospace"><a href="../farmers/<?php echo  $f_aadharpdf;?>" target="_blank">View Aadhar PDF</a></button></p>
-    <p>PAN: <?php echo "$f_pan"?></p>
-    <p>PAN PDF: <button class="btn btn-dark text-monospace"><a href="../farmers/<?php echo  $f_panpdf;?>" target="_blank">View PAN PDF</a></button></p>
-    <p>Status: <?php if($f_approve=="0"){echo "No Action";}else if($f_approve=="1"){echo " Accepted";}else if($f_approve=="2"){echo "Review";}else if($f_approve=="3"){echo "Rejected";}else if($f_approve=="4"){echo "Resubmitted";}  ?></p>
-    <?php
-    }
-    if($f_type=="organization")
-    {
-    ?>
-    <p><img src="../farmers/<?php echo  $f_photo;  ?>" width="200" height="240"></p>
-    <<p>Type: <?php echo "$f_type"?></p>
-    <p>ID: <?php echo "$f_id"?></p>
-    <p>Name: <?php echo "$f_name"?></p>
-    <p>Mobile: <?php echo "$f_mobile"?></p>
-    <p>Contact Person Name: <?php echo "$f_contactname"?></p>
-    <p>Contact Person Gender: <?php echo "$f_gender"?></p>
-    <p> Contact Person Age: <?php echo "$f_age"?></p>
-    <p>Street: <?php echo "$f_street"?></p>
-    <p>City: <?php echo "$f_city"?></p>
-    <p>State: <?php echo "$f_state"?></p>
-    <p>Pincode: <?php echo "$f_pincode"?></p>
-    <p>Registration Document: <button class="btn btn-dark text-monospace"><a href="../farmers/<?php echo  $f_registration;?>" target="_blank">View Document PDF</a></button></p>
-    <p>PAN: <?php echo "$f_pan"?></p>
-    <p>PAN PDF: <button class="btn btn-dark text-monospace"><a href="../farmers/<?php echo  $f_panpdf;?>" target="_blank">View PAN PDF</a></button></p>
-    <p>Status: <?php if($f_approve=="0"){echo "No Action";}else if($f_approve=="1"){echo " Accepted";}else if($f_approve=="2"){echo "Review";}else if($f_approve=="3"){echo "Rejected";}else if($f_approve=="4"){echo "Resubmitted";}  ?></p>
-    <?php
-    }
-    ?>
-    
-    <form action="statusfarmer.php?f_mobile=<?php echo $f_mobile; ?>" method="post">
-    <?php
-    if($f_approve==0){
-    ?>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
-     <hr>
-    <?php
-    }
-    if($f_approve==1){
-    ?>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
-     <hr>
-    <?php
-    }
-    if($f_approve==2){
-    ?>
-    <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
-     <hr>
-    <?php
-    }
-    if($f_approve==3){
-    ?>
-    <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
-     <hr>
-    <?php
-    }
-    if($f_approve==4){
-    ?>
-    <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
-     <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
-    <hr>
-    <?php
-    }
-    ?>
-    </form>
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-xl-12 col-md-12">
+                <div class="card user-card-full">
+                    <div class="row m-l-0 m-r-0">
+                        <div class="col-md-4 bg-c-lite-green user-profile">
+                            <div class="card-block text-center text-white">
+                                <div class="m-b-25"> <img src="../farmers/<?php echo  $f_photo;?>" width="200" height="240" align="center" class="img-radius" alt="User-Profile-Image"> </div>
+                                <h3 class="f-w-600"><?php echo "$f_name"?></h3>
+                                <h5>Farmer</h5>
+								<h5>Status: <?php if($f_approve=="0"){echo "No Action";}else if($f_approve=="1"){echo " Accepted";}else if($f_approve=="2"){echo "Review";}else if($f_approve=="3"){echo "Rejected";}else if($f_approve=="4"){echo "Resubmitted";}  ?></h5>
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <div class="card-block">
+                                <h4 class="m-b-20 p-b-5 b-b-default f-w-600"><strong>Information</strong></h4>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Id</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_id"?></h6>
+                                    </div>
+									<div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Name</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_name"?></h6>
+                                    </div>
+									<div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Mobile</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_mobile"?></h6>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Gender</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_gender"?></h6>
+                                    </div>
+									<div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Age</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_age"?></h6>
+                                    </div>
+                                </div>
+                                <h4 class="m-b-20 m-t-40 p-b-5 b-b-default  f-w-600"><strong>Address</strong></h4>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Street</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_street"?></h6>
+                                    </div>
+									<div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">City</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_city"?></h6>
+                                    </div>
+									<div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">State</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "f_state"?></h6>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Pincode</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_pincode"?></h6>
+                                    </div>
+                                </div>
+                  
+                                <h4 class="m-b-20 m-t-40 p-b-5 b-b-default  f-w-600"><strong>Address</strong></h4>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Aadhaar</p>
+                                        <h6 class="text-muted  f-w-400"><?php echo "$f_aadhar"?> &nbsp; <button class="btn btn-grey text-monospace"><a href="../customers/<?php echo  $f_aadharpdf;?>" target="_blank">View Aadhar</a></button></h6>
+                                    </div>
+									<div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">PAN</p>
+                                        <h6 class="text-muted f-w-400"><?php echo "$f_pan"?> &nbsp; <button class="btn btn-grey text-monospace"><a href="../customers/<?php echo  $f_panpdf;?>" target="_blank">View PAN</a></button></h6>
+                                    </div>
+								</div>
+								
+								 <form action="statusfarmer.php?f_mobile=<?php echo $f_mobile; ?>" method="post">
+									<?php
+								if($f_approve==0){
+								?>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace   f-w-400" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
+								 <hr>
+								<?php
+								}
+								if($f_approve==1){
+								?>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace   f-w-400" style="background-color:#968b0c;"  name="review">Review</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
+								 <hr>
+								<?php
+								}
+								if($f_approve==2){
+								?>
+								<td data-label="Profile"> <button class="btn btn-dark text-monospace   f-w-400" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
+								 <hr>
+								<?php
+								}
+								if($f_approve==3){
+								?>
+								<td data-label="Profile"> <button class="btn btn-dark text-monospace   f-w-400" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
+								 <hr>
+								<?php
+								}
+								if($f_approve==4){
+								?>
+								<td data-label="Profile"> <button class="btn btn-dark text-monospace   f-w-400" style="background-color:#0c3823;"  name="accept">Accept</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#968b0c;"  name="review">Review</button> </td>
+								 <td data-label="Profile"> <button class="btn btn-dark text-monospace" style="background-color:#780611;"  name="reject">Reject</button> </td>
+								<hr>
+								<?php
+								}
+								?>
+								</form>
+									
+							</div>
+									
+                          </div>
+								
+	
+															
+									                   <!--  <ul class="social-link list-unstyled m-t-40 m-b-10">
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                                </ul> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    
-    <div class="footer-dark" style="background: rgb(12,56,35);">
+</div>
+</div>
+</div>
+</div>
+            </div></div>
+<div class="footer-dark" style="background: rgb(12,56,35);">
         <footer>
             <div class="container">
                 <p style="text-align: center;"><strong>© 2021 TapShip.&nbsp; All rights reserved.</strong><br></p>
             </div>
         </footer>
-    </div>
-    <script src="../assets/js/jquery.min.js"></script>
+ </div>
+	<script src="../assets/js/jquery.min.js"></script>
     <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="../assets/js/bs-init.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
     <script src="../assets/js/freelancer.js"></script>
-</body>
-
+  </body>
 </html>
