225605529defe18bdc8a71dd284fe398d16ecb8b
ganpat-patel-012 <ganpat.patel.012@gmail.com>
2021-04-21 14:36:40 +0530

    Update to weather 2

--- farmers/index.php
+++ farmers/index.php
@@ -148,7 +148,7 @@ error_reporting(0);
 
         <div class="container">
             <div class="row" style="color:white; margin:10px; ">
-            <div class="col-lg-1 col-md-12 col-sm-12" style="background-color:#7e090f; padding:20px;">
+            <div class="col-lg-1 col-md-12 col-sm-12" style="background-color:#3d0107; padding:20px;">
                 <h5 class="lap">W</h5>
                 <h5 class="lap">E</h5>
                 <h5 class="lap">A</h5>
@@ -158,7 +158,7 @@ error_reporting(0);
                 <h5 class="lap">R</h5>
                 <h4 class="mob">Weather Report</h4>
             </div>
-                <div class="col-lg-3 col-md-12 col-sm-12" style="background-color:#092b1b; padding:20px;  height: 265px;">
+                <div class="col-lg-3 col-md-12 col-sm-12" style="background-color:#1e5239; padding:20px;  height: 265px;">
                     <h5><?php echo $op[0]; ?></h5>
                     <span><strong><?php echo $op[1]; ?></strong></span><br>
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin location-icon">
@@ -170,7 +170,7 @@ error_reporting(0);
                     <h1><?php echo $op[4]; ?></h1>
                     <h5><?php echo $op[5]; ?></h5>
                 </div>
-                <div class="col-lg-5 col-md-12 col-sm-12" style="background-color:#1e5239; padding:20px; height: 265px;">
+                <div class="col-lg-5 col-md-12 col-sm-12" style="background-color:#092b1b; padding:20px; height: 265px;">
                     <h5>Clouds &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: <?php echo $op[12]; ?> </h5>
                     <h5>Humidity &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: <?php echo $op[11]; ?> </h5>
                     <h5>Feels Like &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: <?php echo $op[10]; ?> </h5>
@@ -179,7 +179,7 @@ error_reporting(0);
                     <h5>Sunrise Time &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: <?php echo $op[6]; ?> </h5>
                     <h5>Sunset Time &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: <?php echo $op[7]; ?> </h5>
                 </div>
-                <div class="col-lg-3 col-md-12 col-sm-12" style="background-color:#092b1b; padding:20px;  height: 265px;">
+                <div class="col-lg-3 col-md-12 col-sm-12" style="background-color:#1e5239; padding:20px;  height: 265px;">
                     <h5>Weather Forcast</h5>
                     <button type="button" class="btn btn-primary">View Forcast</button>
                     <br><br><br>
