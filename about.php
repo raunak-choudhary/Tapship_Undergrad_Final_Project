833b8807f2afee7aadf151350b3db2b06117a146
Ashutosh Kumar <Ashutosh.17cs011@sode-edu.in>
2021-04-23 19:33:44 +0530

    aboutus

---
+++ about.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>About Us</title>
+    <link rel="icon" href="assets/img/fav.png" type="image/png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/card-hover.css">
+    <link rel="stylesheet" href="assets/css/Features-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/Highlight-Blue.css">
+    <link rel="stylesheet" href="assets/css/faq.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.css">
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top text-uppercase" id="mainNav" style="background: #0c3823;">
+        <div class="container-fluid">
+            <a class="navbar-brand js-scroll-trigger" data-bs-hover-animate="pulse" href="index.php" style="font-family: Montserrat, sans-serif;">TAPSHIP</a>
+            <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right text-uppercase rounded" data-aos="fade" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="background: #fff;"><i class="fa fa-bars" style="color: #0c3823;;"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" data-bs-hover-animate="pulse" href="index.php" style="filter: contrast(100%) grayscale(0%) hue-rotate(0deg) invert(0%) sepia(0%);">HOME</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" data-bs-hover-animate="pulse" href="contact.php">CONTACT</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" data-bs-hover-animate="pulse" href="about.php">About</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a href="login-choice.php"><button class="btn btn-dark text-monospace" data-bs-hover-animate="pulse" type="button" style="margin: 10px;background: rgb(255,255,255);color: #0c3823;margin-left: 0;border-radius: 10px;">Log in</button></a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a href="signup-choice.php"><button class="btn btn-dark text-monospace" data-bs-hover-animate="pulse" type="button" style="margin: 10px;background: rgb(255,255,255);color: #0c3823;margin-left: 0;border-radius: 10px;">Sign Up</button></a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a href="admin/login.php"><button class="btn btn-dark text-monospace" data-bs-hover-animate="pulse" type="button" style="margin: 10px;background: rgb(255,255,255);color: #0c3823;margin-left: 0;border-radius: 10px;">Admin</button></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="uppersection">
+        <div class="container text-center">
+            <div class="row mt-5">
+                <div class="col" data-aos="fade">
+                    <h1>Problems we saw</h1>
+                    <p>Farmers are not able to sell their crops to anyone they have to visit sell it to agents in APMC markets.<br>
+                        Agents buy crop at less rate but increased rate the rate when selling to wholesalers.<br>
+                        Truck drivers are not getting enough transport deals due to lack advance infrastructure.<br>
+                    </p>
+                </div>
+            </div>
+            <div class="row my-3">
+                <div class="col" data-aos="fade">
+                    <h1>Our Technology</h1>
+                    <p>We are providing a e-platform to Farmers where they can sell their crops to anyone on their desired rate and conditions.<br>
+                        They will not bounded to sell crop to APMC agents who usually rob them with low price to their crops.<br>
+                        On the other hand Wholesaler who buys from these APMC agents can directly buy from farmers.<br> It will remove the cost added by APMC agents.
+                        And, for transport Truck Drivers can see the successful deals on platform and can transport the crop from Farmers to Wholesaler.
+                        <br>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col" data-aos="fade">
+                    <h1>Origin Story</h1>
+                    <p>
+                        e are providing a e-platform to Farmers where they can sell their crops to anyone on their desired rate and conditions.<br>
+                        They will not
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="team">
+        <div class="container  text-center">
+            <div class="row mb-5">
+                <div class="col" data-aos="fade">
+                    <h1>Our Team</h1>
+                    <div class="p mt-3">this is our team which work hard to make all this happen </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card" data-aos="fade-up">
+                        <div class="card-body">
+                            <img src="assets/img/team/a.jpeg" alt="" class="img-fluid rounded-circle w-60 mb-4">
+                            <h3>Ashutosh kumar</h3>
+                            <h5>UI/UX, Cyber Secrity</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore atque ut totam hic odio quia laudantium voluptate omnis, harum a animi nemo vero, quibusdam id illum dolores nihil cumque magnam?</p>
+                            <div class="d-flex flex-row justify-content-center">
+                                <div class="p-3"><a href="#"><i class="fa fa-facebook"></i></a></div>
+                                <div class="p-3"><a href="#"><i class="fa fa-instagram"></i></a></div>
+                                <div class="p-3"><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                                <div class="p-3"><a href="#"><i class="fa fa-twitter"></i></a></div>
+                                <div class="p-3"><a href="#"><i class="fa fa-youtube"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card" data-aos="fade-up">
+                        <div class="card-body" data-aos="fade">
+                            <img src="assets/img/team/ganni.jpeg" alt="" class="img-fluid rounded-circle w-70 mb-3">
+                            <h3>Ganpat Patel</h3>
+                            <h5>Full Stack and Ml</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore atque ut totam hic odio quia laudantium voluptate omnis, harum a animi nemo vero, quibusdam id illum dolores nihil cumque magnam?</p>
+                            <div class="d-flex flex-row justif-content-center">
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </div>
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </div>
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card" data-aos="fade-up">
+                        <div class="card-body" data-aos="fade">
+                            <img src="assets/img/team/ganni.jpeg" alt="" class="img-fluid rounded-circle w-70 mb-3">
+                            <h3>Ganpat Patel</h3>
+                            <h5>Full Stack and Ml</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore atque ut totam hic odio quia laudantium voluptate omnis, harum a animi nemo vero, quibusdam id illum dolores nihil cumque magnam?</p>
+                            <div class="d-flex flex-row justif-content-center">
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </div>
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </div>
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card" data-aos="fade-up">
+                        <div class="card-body" data-aos="fade">
+                            <img src="assets/img/team/RC.jpg" alt="" class="img-fluid rounded-circle w-70 mb-3">
+                            <h3>Raunak Choudhary</h3>
+                            <h5>ui ux cyber secrity</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore atque ut totam hic odio quia laudantium voluptate omnis, harum a animi nemo vero, quibusdam id illum dolores nihil cumque magnam?</p>
+                            <div class="d-flex flex-row justif-content-center">
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </div>
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </div>
+                                <div class="p-4">
+                                    <a href="#">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <div class="footer-dark" style="background: rgb(12,56,35); margin-top: 50px;">
+        <footer>
+            <div class="container-fluid">
+                <p style="text-align: center;"><strong>© 2021 TapShip.&nbsp; All rights reserved.</strong><br></p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/freelancer.js"></script>
+</body>
+
+</html>
