af1fad23c925da15c37186a9fcb4f3266ed27525
Faheem Ahmad <42387113+faheemahmad227@users.noreply.github.com>
2021-05-10 12:34:29 +0530

    Update viewtransportbidsdetails.php

--- customers/viewtransportbidsdetails.php
+++ customers/viewtransportbidsdetails.php
@@ -29,6 +29,7 @@ $con = mysqli_connect("localhost", "root", "", "tapship");
     <link rel="stylesheet" href="../assets/css/Features-Boxed.css">
     <link rel="stylesheet" href="../assets/css/Footer-Dark.css">
     <link rel="stylesheet" href="../assets/css/Highlight-Blue.css">
+	<link rel="stylesheet" href="../assets/css/profile.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.css">
     <link rel="stylesheet" href="../assets/css/Login-Form-Clean.css">
@@ -102,41 +103,141 @@ $con = mysqli_connect("localhost", "root", "", "tapship");
 
     ?>
 
-    <img src="../farmers/<?php echo  $cr_img1; ?>" width="30%">
-    <img src="../farmers/<?php echo  $cr_img2; ?>" width="30%">
-    <img src="../farmers/<?php echo  $cr_img3; ?>" width="30%">
+	
+        <div>
+            <div class="container">
+                <div class="cust_bloglistintro">
+                    <p style="margin-left:34px;color:rgba(255,255,255,0.5);font-size:14px;"></p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 cust_blogteaser" data-bs-hover-animate="bounce" style="padding-bottom:20px;margin-bottom:32px;"><a href="#"><img class="img-fluid" style="width:100%;" src="../farmers/<?php echo  $cr_img1; ?>"></a>
 
-    <h5>Crop Details</h5>
-    <p>Crop ID: <?php echo $cro_id; ?></P>
-    <p>Crop Name: <?php echo $cro_name; ?></P>
-    <p>Crop Type: <?php echo $cro_type; ?></P>
-    <p>Crop Sale ID: <?php echo $cr_id; ?></P>
-    <p>Quantity: <?php echo $cr_quantity, ' Kgs'; ?></P>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 cust_blogteaser" style="padding-bottom:20px;margin-bottom:32px;"><a href="#"><img class="img-fluid" data-bs-hover-animate="bounce" style="width:100%;" src="../farmers/<?php echo  $cr_img2; ?>"></a>
+                        <a class="h4" href="#"></a>
+                    </div>
+                    <div class=" col-lg-4 col-md-12 col-sm-12 col-xs-12 cust_blogteaser" style="padding-bottom:20px;margin-bottom:32px;"><a href="#"><img class="img-fluid" data-bs-hover-animate="bounce" style="width:100%;" src="../farmers/<?php echo  $cr_img3; ?>"></a>
+                        <a class="h4" href="#"></a>
+                    </div>
+                </div>
 
-    <h5>Farmer Details</h5>
-    <p>Farmer Name: <?php echo $f_name; ?></P>
-    <p>Farmer Mobile: <?php echo $f_mobile; ?></P>
-    <p>Farmer Gender: <?php echo $f_gender; ?></P>
-    <p>Farmer Age: <?php echo $f_age; ?></P>
-    <p>Farmer Street: <?php echo $f_street; ?></P>
-    <p>Farmer City: <?php echo $f_city; ?></P>
-    <p>Farmer State: <?php echo $f_state; ?></P>
-    <p>Farmer Pincode: <?php echo $f_pincode; ?></P>
+            </div>
+	<div class="padding">
+                <div class="row container d-flex justify-content-center">
+                    <div class="col-xl-12 col-md-12">
+                        <div class="card user-card-full">
+                            <div class="row m-l-0 m-r-0">
 
-    <h5>Driver Details</h5>
-    <p>Driver Name: <?php echo $d_name; ?></P>
-    <p>Driver Mobile: <?php echo $d_mobile; ?></P>
-    <p>Driver Gender: <?php echo $d_gender; ?></P>
-    <p>Driver Age: <?php echo $d_age; ?></P>
-    <p>Driver Street: <?php echo $d_street; ?></P>
-    <p>Driver City: <?php echo $d_city; ?></P>
-    <p>Driver State: <?php echo $d_state; ?></P>
-    <p>Driver Pincode: <?php echo $d_pincode; ?></P>
-
-    <h5>Transport Details</h5>
-    <p>Transport ID: <?php echo $tb_id; ?></P>
-    <p>Transport Bid: <?php echo $tb_bid; ?></P>
-    <p>Transport Status: <?php echo $tb_status; ?></P>
+                                <div class="col-sm-12 col-md-12 col">
+                                    <div class="card-block">
+                                        <h4 class="m-b-20 p-b-5 b-b-default f-w-600"><strong>Crop Details</strong></h4>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Crop ID</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$cro_id" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Crop Name</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$cro_name" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Crop Type</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$cro_type" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Crop Sale ID</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$cr_id" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Quantity</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$cr_quantity Kgs" ?></h6>
+                                            </div>
+										 </div><br>
+                                        <h4 class="m-b-20 p-b-5 b-b-default f-w-600"><strong>Farmer Details</strong></h4>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer Name</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_name" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer Mobile</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_mobile" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer Gender</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_gender" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer Age</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_age" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer Street</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_street" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer City</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_city" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer State</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_state" ?></h6>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <p class="m-b-10 f-w-600">Farmer Pincode</p>
+                                                <h6 class="text-muted f-w-400"><?php echo "$f_pincode" ?></h6>
+                                            </div>
+                                        </div><br>
+										<h4 class="m-b-20 p-b-5 b-b-default f-w-600"><strong>Driver Details</strong></h4>
+                                            <div class="row">
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Driver Name</p>
+                                                    <h6 class="text-muted f-w-400"><?php echo "$d_name" ?></h6>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Driver Mobile</p>
+                                                    <h6 class="text-muted f-w-400"><?php echo "$d_mobile" ?></h6>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Driver Age</p>
+                                                    <h6 class="text-muted f-w-400"><?php echo "$d_age" ?></h6>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Driver Gender</p>
+                                                    <h6 class="text-muted f-w-400"><?php echo "$d_gender" ?></h6>
+                                                </div>
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Driver Street</p>
+													<h6 class="text-muted f-w-400"><?php echo "$d_street" ?></h6>
+												</div>
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Driver City</p>
+													<h6 class="text-muted f-w-400"><?php echo "$d_city" ?></h6>
+												</div>
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Driver State</p>
+													<h6 class="text-muted f-w-400"><?php echo "$d_state" ?></h6>
+												</div>
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Driver Pincode</p>
+													<h6 class="text-muted f-w-400"><?php echo "$d_pincode" ?></h6>
+												</div>
+											</div><br>
+											<h4 class="m-b-20 p-b-5 b-b-default f-w-600"><strong>Tranport Details</strong></h4>
+											<div class="row">
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Tranport ID</p>
+													<h6 class="text-muted f-w-400"><?php echo $tb_id; ?></h6>
+												</div>
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Tranport Bid</p>
+													<h6 class="text-muted f-w-400"><?php echo $tb_bid; ?></h6>
+												</div>
+												<div class="col-sm-6">
+													<p class="m-b-10 f-w-600">Tranport Status</p>
+													<h6 class="text-muted f-w-400"><?php echo $tb_status; ?></h6>
+												</div>
+											</div><br>
 
     <?php
     if ($cr_status == 7) {
@@ -146,6 +247,15 @@ $con = mysqli_connect("localhost", "root", "", "tapship");
             <hr>
         </form>
     <?php } ?>
+	
+	 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
 
     <div class="footer-dark" style="background: rgb(12,56,35);">
         <footer>
