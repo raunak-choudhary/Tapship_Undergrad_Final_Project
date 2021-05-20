1db184570c202e3ba257eb42cd72ec8111048406
Raunak Chaudhary <63695296+raunak-choudhary@users.noreply.github.com>
2021-05-20 19:47:09 +0530

    Auth Token again CHange

--- api/twilio/config.php
+++ api/twilio/config.php
@@ -1,4 +1,4 @@
 <?php
 $account_sid = 'AC6d802a41dd5de2ceff835422a6de12fc';
-$auth_token = '05ba03ae79c2b462f184c244921684b1';
+$auth_token = '33ea4dda946dac664760745d8030dd89';
 $twilio_number = "+12568874154";
