0267420da5ed683391a7d86c8a1c823503120796
Raunak Chaudhary <63695296+raunak-choudhary@users.noreply.github.com>
2021-10-01 18:50:46 +0530

    next update

--- api/twilio/config.php
+++ api/twilio/config.php
@@ -1,4 +1,4 @@
 <?php
-$account_sid = 'AC6d802a41dd5de2ceff835422a6de12fc';
-$auth_token = '33ea4dda946dac664760745d8030dd89';
-$twilio_number = "+12568874154";
+$account_sid = 'AC6a054a3c203bb7af554b5ac02a1f395d';
+$auth_token = '95483dce05c55e902b05941dd43cdbef';
+$twilio_number = "+12679301392";
